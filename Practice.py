2989bf78e90e51554e31449acc34d29a75716a8c
ucheolisa312 <ucheolisa312>
2021-06-08 11:07:26 +0100

    "instructions"

--- Practice.py
+++ Practice.py
@@ -1,7 +1,10 @@
- #Creating class in python
- #Syntax is as follows:
- # class classname:
- class Employee:
-     id = 10
-     name = "ayush"
-     def display(self):
+#  #Creating class in python
+#  #Syntax is as follows:
+#  # class classname:
+class Employee:
+    id = 10;
+    name = "ayush"
+    def display(self):
+         print(self.id,self.name)
+# class Employee:
+#     id = 10
