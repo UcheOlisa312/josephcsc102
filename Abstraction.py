933263dd88c8729711a6a0612279880ce8ee2369
ucheolisa312 <ucheolisa312>
2021-06-24 15:44:54 +0100

    "instructions"

--- Abstraction.py
+++ Abstraction.py
@@ -20,20 +20,38 @@
 # print("Area of a circle:", cir.calculate_area())
 
 # Abstraction example 2
-from abc import ABC , abstractmethod
-class Shape(ABC):
-    def printx(self):
-        print("I am a normal method defined inside the abstract class 'Shape'")
+# from abc import ABC , abstractmethod
+# class Shape(ABC):
+#     def printx(self):
+#         print("I am a normal method defined inside the abstract class 'Shape'")
+#
+#     @abstractmethod
+#     def calculate_area(self):
+#         pass
+# class Rectangle(Shape):
+#   length = 5
+#   breadth = 3
+#   def calculate_area(self):
+#     return self.length * self.breadth
+# rec = Rectangle()
+# rec.printx()
+# print("Area of a rectangle:", rec.calculate_area())
 
+# IN-CLASS EXERCISE
+from abc import ABC, abstractmethod
+class  Bird(ABC):
+    def defecate(self):
+        print(" I can defecate")
     @abstractmethod
-    def calculate_are(self):
+    def move(self):
         pass
-class Rectangle(Shape):
-  length = 5
-  breadth = 3
-  def calculate_area(self):
-    return self.length * self.breadth
+class Hen(Bird):
+    def move(self):
+        return " what kind of move?"
+class Ostrich(Bird):
+    def move(self):
+        return "Pls, what kind of move?"
 
-rec = Rectangle()
-rec.printx()
-print("Area of a rectangle:", rec.calculate_area())
+hen = Hen()
+hen.defecate()
+
